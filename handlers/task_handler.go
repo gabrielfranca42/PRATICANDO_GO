f0ff9cf6355a250d4e3bdcbe3dafc7b2e709262d
Gabriel Eduardo de Moura Franca <gabrielfranca172@gmail.com>
2026-01-20 09:04:35 -0300

    task_handler.go finalizado

--- handlers/task_handler.go
+++ handlers/task_handler.go
@@ -2,7 +2,10 @@ package handlers
 
 import (
 	"database/sql"
+	"encoding/json"
 	"net/http"
+
+	"github.com/gabrielfranca42/simple-go-mod/models"
 )
 
 type TaskHandler struct {
@@ -13,9 +16,31 @@ func NewTaskHandler(db *sql.DB) *TaskHandler {
 	return &TaskHandler{DB: db}
 }
 
-func (taskHandler *TaskHandler) ReadTasks(w http.ResponseWriter, request *http Request) {
+func (taskHandler *TaskHandler) ReadTasks(w http.ResponseWriter, request *http.Request) {
 	rows, err := taskHandler.DB.Query("SELECT * FROM tasks")
 	if err != nil {
-		http.Error(writer, err Error(), )
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
+
+	var tasks []models.Task
+
+	for rows.Next() {
+		var task models.Task
+		err := rows.Scan(&task.ID, &task.Title, &task.Description, &task.Status)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		tasks = append(tasks, task)
+
+	}
+
+	tasks = append(tasks, task)
+
+	w.Header().Set("content-type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+
 }
